896e1c95ae0939ec64008555721c1a4ac1312513
Fredrik Ekholm <fredrik.w.ekholm@gmail.com>
2023-11-06 16:15:03 +0000

    save checkpoint regularly

--- train.py
+++ train.py
@@ -237,6 +237,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations,save_ever
                 scene.save(iteration)
             if save_every is not None and iteration%save_every == 0:
                 scene.save("current")
+                torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt_current.pth")
 
             # Densification
             if iteration < opt.densify_until_iter and gaussians._xyz.shape[0] < opt.max_gaussians:
